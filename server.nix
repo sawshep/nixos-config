99ab86fd25d76645ee36d5d4ec691a35e40b63c8
Sawyer Shepherd <contact@sawyershepherd.org>
2023-08-18 18:16:01 -0400

    Update allowed SSH keys

--- server.nix
+++ server.nix
@@ -7,6 +7,10 @@
     isNormalUser = true;
     initialPassword = "password";
     extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHe4JdAXSDsJFeVAlY9vq+y3mFDZIPoBArAIfgt38vEW" # Firewall user
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHshr48iQqsn1H5ErCNVIdxaLMyt5X//ZRuhMg+WIVfq" # Firewall root
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJd0gD46Ddn2Bsl0+MIxkxO8AyupYfXj2Y9Z6xOnkMlt" # Laptop user
   };
 
   services.openssh = {
