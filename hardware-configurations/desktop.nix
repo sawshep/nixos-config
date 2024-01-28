07681344543dd62d50b340280536749ec6c2203f
Sawyer Shepherd <contact@sawyershepherd.org>
2024-01-28 17:15:12 -0500

    Remove crashing service

--- hardware-configurations/desktop.nix
+++ hardware-configurations/desktop.nix
@@ -46,14 +46,6 @@
     driSupport32Bit = true;
   };
 
-    systemd.services.nvidia-control-devices = {
-    wantedBy = [
-      "multi-user.target"
-    ];
-    serviceConfig.ExecStart = "${config.boot.kernelPackages.nvidiaPackages.beta}/bin/nvidia-smi";
-  };
-
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
