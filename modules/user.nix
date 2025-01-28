4f2e41cc3247f2c43b50883bf949d5da3c08e597
Sawyer Shepherd <contact@sawyershepherd.org>
2025-01-28 00:05:42 -0500

    Add services

--- modules/user.nix
+++ modules/user.nix
@@ -16,7 +16,21 @@ in
 
   hardware.bluetooth.enable = true;
 
+  location.provider = "geoclue2";
+
   services = {
+    redshift = {
+      enable = true;
+      brightness = {
+        # Note the string values below.
+        day = "1";
+        night = "1";
+      };
+      temperature = {
+        day = 5500;
+        night = 3700;
+      };
+    };
     xserver = {
       enable = true;
       excludePackages = [ pkgs.xterm ];
