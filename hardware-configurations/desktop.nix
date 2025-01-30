0cf644b0d17726a1ecd0f5389db5c643004c0f9c
Sawyer Shepherd <contact@sawyershepherd.org>
2025-01-30 00:25:25 -0500

    Add local LLMs

--- hardware-configurations/desktop.nix
+++ hardware-configurations/desktop.nix
@@ -39,7 +39,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
   hardware.graphics = {
     enable = true;
--- workstation.nix
+++ workstation.nix
@@ -13,14 +13,48 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
+  systemd.services.nvidia-control-devices = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";
+  };
+
   nixpkgs.config.nvidia.acceptLicense = true;
 
   networking.hostName = "codebreaker"; # Define your hostname.
 
   networking.hosts = {
-    "10.0.0.4" = ["radicale.spaceheaterlab.net"];
-    "10.0.0.6" = ["manage.spaceheaterlab.net"];
+    "10.0.0.5" = ["radicale.spaceheaterlab.net"];
+    "10.0.0.6" = ["lab.cyberhawks.org"];
   };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 443 ];
+  };
+
+  services.syncthing.openDefaultPorts = true;
+
+  services.ollama = {
+    enable = true;
+    environmentVariables = {
+      CUDA_VISIBLE_DEVICES = "0,1";
+      OLLAMA_LLM_LIBRARY = "cuda_v11";
+    };
+    acceleration = "cuda";
+  };
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."10.0.0.24" = {
+        extraConfig = ''
+          tls internal
+          reverse_proxy http://localhost:8080
+        '';
+      };
+  };
+
+  services.open-webui.enable = true;
+  services.open-webui.openFirewall = true;
 
   services.hardware.openrgb = {
     enable = true;
@@ -29,6 +63,16 @@
     #server = {
     #  port = 6742;
     #};
+  };
+
+  nixpkgs.config.cudaSupport = true;
+  nix.settings = {
+    substituters = [
+      "https://cuda-maintainers.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
   };
 
 
@@ -62,11 +106,20 @@
   environment.systemPackages = with pkgs; [
 
     teamviewer
+
+    cudaPackages.cuda_gdb
     cudaPackages.cudatoolkit
-    #libGLU libGL
-    #linuxPackages.nvidia_x11
-    #xorg.libXi xorg.libXmu freeglut
-    #xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib
+    cudaPackages.cudnn
+    cudaPackages.cutensor
+    cudaPackages.libcublas
+    cudaPackages.libcufft
+    cudaPackages.libcufile
+    cudaPackages.libcurand
+    cudaPackages.libnpp
+    cudaPackages.libnvjpeg
+    cudaPackages.nvidia_fs
+    cudaPackages.saxpy
+
 
     exiftool
     ollama
