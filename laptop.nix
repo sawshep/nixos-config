59fbd5b3a478e052f239feca06629802e588e06b
Sawyer Shepherd <contact@sawyershepherd.org>
2026-01-28 14:27:48 -0500

    laptop fingerprint reader

--- laptop.nix
+++ laptop.nix
@@ -37,6 +37,9 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
+  services.fprintd = {
+    enable = true;
+  };
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
